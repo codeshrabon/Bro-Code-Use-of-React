bd8cd5dfda078b3e50f28ca238219c0b2dcb5687
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-20 19:19:36 +0600

    check your import files if you have imported them or not

--- Bro Code Expriment/my-expriment-project/src/Apps.jsx
+++ Bro Code Expriment/my-expriment-project/src/Apps.jsx
@@ -1,16 +1,17 @@
 import Lists from "./Lists/Lists"
 function Apps(){
-const fruits = [
-          {id:10,name:"banana", buyer:"canada"},
-         {id:11,name:"apple" , buyer: "USA"}, 
-         {id:12,name :"mango", buyer: "sauth africa"}, 
-         {id:13,name :"orange", buyer: "austrelia"}, 
-         {id:14,name: "pinapple", buyer: "china"}, 
-         {id:15,name: "guava", buyer: "uganda"}];
 
-         return(
-            <Lists />
-         );
+    const fruit = [
+            {id:10,name:"banana", buyer:"canada"},
+            {id:11,name:"apple" , buyer: "USA"}, 
+            {id:12,name :"mango", buyer: "sauth africa"}, 
+            {id:13,name :"orange", buyer: "austrelia"}, 
+            {id:14,name: "pinapple", buyer: "china"}, 
+            {id:15,name: "guava", buyer: "uganda"}];
+
+            return(
+                <Lists items={fruit} category="Fruits" />
+            );
 }
 
 export default Apps
--- Bro Code Expriment/my-expriment-project/src/Lists/Lists.jsx
+++ Bro Code Expriment/my-expriment-project/src/Lists/Lists.jsx
@@ -1,6 +1,15 @@
 function Lists(props){
 
 
+    const listOfItems = props.items;
+
+    const listItems = listOfItems.map(item => <li key={item.id}>
+                                                        {item.name}: &nbsp;
+                                                        <b>{item.buyer}</b>
+                                                        </li> );
+
+        return( <ol>{listItems}</ol> );
+
 
 
 }
