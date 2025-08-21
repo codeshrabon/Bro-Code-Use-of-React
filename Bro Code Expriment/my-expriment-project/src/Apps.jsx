03342a9eb468a39aff886cc6e5a87484ab2436b3
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-21 12:56:43 +0600

    use PropTypes to avoid data type misplace - get warning , use defaultProps to get category or anything as you like with out getting blank display

--- Bro Code Expriment/my-expriment-project/src/Apps.jsx
+++ Bro Code Expriment/my-expriment-project/src/Apps.jsx
@@ -2,15 +2,35 @@ import Lists from "./Lists/Lists"
 function Apps(){
 
     const fruit = [
-            {id:10,name:"banana", buyer:"canada"},
-            {id:11,name:"apple" , buyer: "USA"}, 
-            {id:12,name :"mango", buyer: "sauth africa"}, 
-            {id:13,name :"orange", buyer: "austrelia"}, 
-            {id:14,name: "pinapple", buyer: "china"}, 
-            {id:15,name: "guava", buyer: "uganda"}];
+            {id:1,name:"banana", buyer:"canada"},
+            {id:2,name:"apple" , buyer: "USA"}, 
+            {id:3,name :"mango", buyer: "sauth africa"}, 
+            {id:4,name :"orange", buyer: "austrelia"}, 
+            {id:5,name: "pinapple", buyer: "china"}, 
+            {id:6,name: "guava", buyer: "uganda"}
+        ];
+    
+    const vegetables = [
+            {id:7, name:"potatos", buyer:"USA"},
+            {id:8,name:"cucumber" , buyer: "Canada"}, 
+            {id:9,name :"ladyfingers", buyer: "North africa"}, 
+            {id:10,name :"Red chili", buyer: "Belgium"}, 
+            {id:11,name: "paper", buyer: "Malesiya"}, 
+            {id:12,name: "carrots", buyer: "Africa"}
+        ];
 
             return(
-                <Lists items={fruit} category="Fruits" />
+
+                <>
+                {/* Here is a catch if we dont want to use tenary operator 
+                    we can also use && operation ... cause data is always true 
+                */}
+                {fruit.length > 0 && <Lists items={fruit} category="Fruits" /> }
+               {vegetables.length > 0 && <Lists items={vegetables} category="Vegetables" />}
+
+               {/* We need to add some default prompts incase we dont have data  */}
+                </>
+                
             );
 }
 
--- Bro Code Expriment/my-expriment-project/src/Lists/Lists.jsx
+++ Bro Code Expriment/my-expriment-project/src/Lists/Lists.jsx
@@ -1,17 +1,41 @@
+import PropTypes from 'prop-types'
+
 function Lists(props){
 
 
+    const categorys = props.category;
     const listOfItems = props.items;
 
     const listItems = listOfItems.map(item => <li key={item.id}>
                                                         {item.name}: &nbsp;
                                                         <b>{item.buyer}</b>
                                                         </li> );
+        //listOfItems.sort((a,b) => a.name.localeCompare(b.name))
+            
+        return( <>
+            <h2 className="list-category">{categorys}</h2>
+            <ol className="list-items">{listItems}</ol>
+        
+        </> );
 
-        return( <ol>{listItems}</ol> );
 
 
+}
+//setup a propTypes if someone use wrong data type 
+Lists.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            name: PropTypes.string,
+            buyer: PropTypes.string
+        })
+    )
+}
 
+Lists.defaultProps ={
+    category : "Category",
+    items :[],
 }
 
 export default Lists
