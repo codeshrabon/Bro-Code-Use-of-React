1aa864d34d60edc19b18e4a04f18eaa60197c29b
Faysal Ahmed <19101043@uap-bd.edu>
2025-08-21 15:42:29 +0600

    using parameter inside eventhandler to see how many time you click them and giving them a limit

--- Bro Code Expriment/my-expriment-project/src/ClickEvents/Button.jsx
+++ Bro Code Expriment/my-expriment-project/src/ClickEvents/Button.jsx
@@ -6,10 +6,25 @@ function Button(){
     // template string ${expression} of string 
     const handleClick2 = (name) => console.log(`${name} Stop it`);
 
+    // count how many time the button has been clicked 
+    let count = 0;
+
+    const handleChicl3 = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} times`);
+
+        }else{
+            console.log(`${name} stop clicking me`);
+        }
+    }
+
+
     return (
         <>
         <button onClick={handleClick}>Click me</button>
         <button onClick={() => handleClick2("Faysal")}>Click me and watch</button>
+        <button onClick={() => handleChicl3("Faysal")}>Click and see</button>
         </>
     );
 
